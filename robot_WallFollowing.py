04fb6ca98a9800ed951acdecbaee5f2e8926b10f
Camila Pluas <camilapluas@usf.edu>
2025-10-02 15:36:16 -0400

    Update robot_WallFollowing.py
    Update

--- robot_WallFollowing.py
+++ robot_WallFollowing.py
@@ -1,98 +1,76 @@
 
-#I will be using the CSP algorithm.
-#find an NxN matrix with numbers from 1 to N showing exactly once in each row and column in a way that the amount of "visible numbers" matches a given input.
-#The very first number will determine the size of the matrix
-#4 lines will describe the number of visible numbers from
-  #top to bottom
-  #bottom to top
-  #left to right
-  #right to left
-#number of visibility will determine the number on that index.
-#create a matrix with that info
+#Task1 - less distance slow down 
 
-# Constraints:
-# All numbers must be unique (no repetition)
+# e(t) = a-d
+# Desired (d) is distance to the wall 
+# actual (a) is the actual distance, gotten from the lidar. degrees 
 
-#if there is only one visible number then the number must be N
+# For lidar (a): do not take the average of the distance, Minimums will be best to use 
+# [choose->123, 153] of these range do not take average 
 
-def dfs(board, r, c, N, top, bottom, left, right):
-    # If reached end of board -> check all constraints
-    if r == N:
-        return check_all_constraints(board, N, top, bottom, left, right)
 
-    # Next cell coordinates
-    nr, nc = (r, c+1) if c+1 < N else (r+1, 0)
-
-    for val in range(1, N+1):
-        board[r][c] = val
-        if is_valid(board, r, c, N, top, bottom, left, right):
-            if dfs(board, nr, nc, N, top, bottom, left, right):
-                return True
-        board[r][c] = 0  # backtrack
-
-    return False
-
-def solve_skyscraper_dfs(N, top, bottom, left, right):
-    board = [[0] * N for _ in range(N)]
-    if dfs(board, 0, 0, N, top, bottom, left, right):
-        return board
-    return None
-
-def count_visible(seq):
-    max_seen, count = 0, 0
-    for x in seq:
-        if x > max_seen:
-            count += 1
-            max_seen = x
-    return count
-
-def check_all_constraints(board, N, top, bottom, left, right):
-    for r in range(N):
-        if count_visible(board[r]) != left[r]: return False
-        if count_visible(board[r][::-1]) != right[r]: return False
-    for c in range(N):
-        col = [board[r][c] for r in range(N)]
-        if count_visible(col) != top[c]: return False
-        if count_visible(col[::-1]) != bottom[c]: return False
-    return True
-
-def is_valid(board, r, c, N, top, bottom, left, right):
-    # Check row uniqueness
-    row = board[r]
-    if len(set([x for x in row if x != 0])) != len([x for x in row if x != 0]):
-        return False
-
-    # Check column uniqueness
-    col = [board[i][c] for i in range(N)]
-    if len(set([x for x in col if x != 0])) != len([x for x in col if x != 0]):
-        return False
-
-    # If row is complete -> check visibility
-    if all(board[r][j] != 0 for j in range(N)):
-        if count_visible(board[r]) != left[r]: return False
-        if count_visible(board[r][::-1]) != right[r]: return False
-
-    # If column is complete -> check visibility
-    col = [board[i][c] for i in range(N)]
-    if all(col):
-        if count_visible(col) != top[c]: return False
-        if count_visible(col[::-1]) != bottom[c]: return False
-
-    return True
+# Phi(t) = k(p)*(e(t))
+# Where phi is angular velocity and k(p) is a choosen variable k(p) = 3 is a good choice 
 
 
 
-if __name__ == "__main__":
-    N = int(input().strip())
-    top = list(map(int, input().split()))
-    bottom = list(map(int, input().split()))
-    left = list(map(int, input().split()))
-    right = list(map(int, input().split()))
+#Task2 - Wall folllowing 
+#USE PID
+#Use range of values from the lidar to follow the wall.
+#we need a variable that will determine how bigof a range we want from the range.
+#Remember to always use the minimum, the minimum will let you know how close the bot is to the wall 
 
-    solution = solve_skyscraper(N, top, bottom, left, right)
 
-    if solution:
-        for row in solution:
-            print(" ".join(map(str, row)))
+# e(s)(t) = |a-d|
+#(change in)P(t) = K(Ps) * e(s)
+#phi(right)(t) = 
+
+#if a < d
+    #
+#if a > b
+def sat(self, v):
+    if v> self.max_motor_velocity:
+        return self.max_motor_velocity
+    elif v< -self.max_motor_velocity:
+        return -self.max_motor_velocity
     else:
-        print("No solution")
+        return v
+
+
+#forward distance  and kp can be changed 
+def forward_PID(self, forward_distance = 0.5, Kp = 3):   #can be used for task 1 and 2 if we change kp then the bot wil start slowing down accordingly 
+    actual = min(self.get_lidar_range_image()[175:185])
+    e= actual - forward_distance
+    forward_v = self.sat(Kp * e)
+    return forward_v
+
+
+def side_PID(self, side_distance=0.1, Kp=3, side = 'Left'):  #side_distance=0.1 <- these values can be an issue
+    if side == 'Left':
+        actual = min(self.get_lidar_range_image()[90:115])
+        e= abs(actual - side_distance)
+        return Kp * e
+    
+def Rotation (sef,degrees):
+    
+
+#bot.move_to_start()
+def main ():
+    forward_distance = min(bot.get_lidar_range_image()[175:185])
+    forward_velocity =  bot.forward_PID()
+    delta_velocity = bot.side_PID()
+    side_distance =min(bot.get_lidar_range_image()[90:115])
+
+    if side_distance < .2: #bot to close to wall
+        left_v = forward_velocity
+        right_v = bot.sat(forward_velocity -delta_velocity)
+
+    elif(side_distance > .2):
+        left_v = bot.sat(forward_velocity -delta_velocity)
+        right_v = forward_velocity
+    else:
+        left_v = forward_velocity
+        right_v = forward_velocity
+
+bot.set_left_motor_velocity(left_v)
+bot.set_right_motor_velocity(right_v)
