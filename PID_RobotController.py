25b104d31a3414a9febf0fa96423bdce0332a1fe
Camila Pluas <camilapluas@usf.edu>
2025-10-08 18:02:57 -0400

    Update PID_RobotController.py

--- PID_RobotController.py
+++ PID_RobotController.py
@@ -1,3 +1,17 @@
+#Task1 - less distance slow down 
+
+# e(t) = a-d
+# Desired (d) is distance to the wall 
+# actual (a) is the actual distance, gotten from the lidar. degrees 
+
+# For lidar (a): do not take the average of the distance, Minimums will be best to use 
+# [choose->123, 153] of these range do not take average 
+
+
+# Phi(t) = k(p)*(e(t))
+# Where phi is angular velocity and k(p) is a choosen variable k(p) = 3 is a good choice 
+
+
 # Constants
 DESIRED_DISTANCE = 1.0   # meters
 KP = 3.0                 # proportional gain
